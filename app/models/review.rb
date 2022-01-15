83ea3e1529cdba6388933954ee7be33fe827e483
Shashi Singh <s.singh510@gmail.com>
2022-01-15 06:02:47 -0500

    validate number of reviews per book

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,15 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  validate :number_of_reviews
+
+  private
+
+  def number_of_reviews
+    return unless reviewable.is_a?(Book)
+    return unless user.reviews.where(reviewable: reviewable).exists?
+
+    errors.add(:user, "can't have more than one review per book.")
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -11,6 +11,35 @@ RSpec.describe Review, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:rating) }
     it { is_expected.to validate_numericality_of(:rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(5) }
+
+    describe '#user' do
+      subject(:review) { build(:review, reviewable: reviewable) }
+
+      context 'when the reviewable is a Book' do
+        let(:reviewable) { create(:book) }
+
+        context 'when the user has no existing reviews for the book' do
+          it { is_expected.to be_valid }
+        end
+
+        context 'when the user has an existing review for the book' do
+          let!(:review) { create(:review, reviewable: reviewable) }
+
+          it { is_expected.not_to be_valid }
+
+          it 'has error message' do
+            subject.valid?
+            expect(subject.errors.full_messages).to contain_exactly "User can't have more than one review per book."
+          end
+        end
+      end
+
+      context 'when the reviewable is NOT a Book' do
+        let(:reviewable) { create(:author) }
+
+        it { is_expected.to be_valid }
+      end
+    end
   end
 
   describe 'factories' do
